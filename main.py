f7c03b9da213b62171efbf1be85100b3c1d542fa
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-19 20:02:43 +0000

    create dictionary for the opposite pit

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import sys
 PLAYER_1_PITS = ('A', 'B', 'C', 'D', 'E', 'F')
 PLAYER_2_PITS = ('G', 'H', 'I', 'J', 'K', 'L')
 
+OPPOSITE_PIT = {'A': 'G', 'B': 'H', 'C': 'I', 'D': 'J', 'E': 'K','F': 'L', 'G': 'A', 'H': 'B', 'I': 'C', 'J': 'D', 'K': 'E', 'L': 'F'}
