3aafd8652ec24e83f8ce1da573d8002a6bc17eb3
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-19 20:10:52 +0000

    create dictionary for next pit

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ PLAYER_1_PITS = ('A', 'B', 'C', 'D', 'E', 'F')
 PLAYER_2_PITS = ('G', 'H', 'I', 'J', 'K', 'L')
 
 OPPOSITE_PIT = {'A': 'G', 'B': 'H', 'C': 'I', 'D': 'J', 'E': 'K','F': 'L', 'G': 'A', 'H': 'B', 'I': 'C', 'J': 'D', 'K': 'E', 'L': 'F'}
+NEXT_PIT = {'A':'B', 'B': 'C', 'C':'D', 'D':'E', 'E':'F', 'F':'1', '1':'L', 'L':'K', 'K': 'J', 'J':'I', 'I':'H', 'H':'G', 'G':'2', '2':'A'}
+
