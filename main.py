4e5d84846cf739e9f393a986ba6d60afdb71ec24
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-19 20:48:47 +0000

    create gameboard and ask player for move

--- main.py
+++ main.py
@@ -24,5 +24,44 @@ def main():
         playerTurn = makeMove(gameBoard, playerTurn, playerMove)
 
         winner = checkforWinner(gameBoard)
+        if winner == '1' or winner == '2':
+            displayBoard(gameBoard)
+            print('Player' + winner + 'has won!')
+            sys.exit()
+        elif winner == 'tie':
+            displayBoard(gameBoard)
+            print('there is a tie!')
+            sys.exit
         
+def getNewBoard():
+    s = STARTING_NUMBERS_OF_SEEDS
+    return {'1':0, '2':0, 'A': s, 'B': s, 'C': s, 'D': s, 'E': s, "F": s, 'G': s, 'H': s, 'I': s, 'J': s, 'K': s, 'L': s }
 
+def displayBoard(board):
+    seedAmounts = []
+    for pit in 'GHIJKL21ABCDEF':
+        numSeedsInThisPit = str(board[pit]).rjust(2)
+        seedAmounts.append(numSeedsInThisPit)
+
+        print("""
+        +------+------+--<<<<<-Player 2---+------+------+-----+
+        2.      |G     |H.     |I.     |J      |K.       |L.   |  1
+                |      |       |.      |       |         |.    |.  
+        S.      |  { } |  { }  |. { }  | { }   | { }     |  { }|.  S
+        T.      |.     |.      |       |.      |.        |     |   T
+        O. { }. +------+-------+-------+-------+---------+-----+.{}O
+        R.      |A.    |B.     |C.     |D.     |E.       | F.  |.  R
+        E.      |  { } |  { }  |. { }  |  { }. |  { }    | { } |   E
+        +-------+------+------PLAYER 1->>>>----+---------+-----+----+
+        """.format(*seedAmounts)) 
+
+def askForPlayerMove(playerTurn, board):
+    while True:
+        if playerTurn == '1':
+            print('Player 1, choose move: A-F (or QUIT)')
+        elif playerTurn =='2':
+            print('Player 2, choose move G- L(or QUIT)')
+        response input('> ').upper().strip()
+        
+        if response == 'QUIT':
+            
